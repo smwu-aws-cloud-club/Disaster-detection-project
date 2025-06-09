f7c2fe961f584fc776351f3df8c4831297320d87
Hanmh111 <96728777+Hanmh111@users.noreply.github.com>
2025-06-10 03:53:47 +0900

    Feat: 탐지 후 region db 변경 람다 함수 추가

---
+++ node.js/index-region-db.mjs
@@ -0,0 +1,83 @@
+import dotenv from "dotenv";
+import {DynamoDBClient} from "@aws-sdk/client-dynamodb";
+import {UpdateCommand, DynamoDBDocumentClient} from "@aws-sdk/lib-dynamodb";
+
+dotenv.config();
+
+const ddbClient = new DynamoDBClient({region: process.env.AWS_REGION});
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
+
+export const handler = async (event) => {
+  let parsedEvent;
+  try {
+    parsedEvent = typeof event.body === "string" ? JSON.parse(event.body) : event;
+  } catch (parseError) {
+    console.error("Failed to parse event body:", parseError);
+    return {
+      statusCode: 400,
+      body: JSON.stringify({message: "Invalid event body format"}),
+    };
+  }
+
+  const region = parsedEvent.region || "불명";
+
+  if (region === "불명") {
+    console.warn("Update request received with undefined or '불명' region.");
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: "업데이트할 지역이 정해져있지 않습니다. 유효한 'region' 값이 필요합니다.",
+      }),
+    };
+  }
+
+  // 명확하게 boolean으로 파싱
+  const status =
+    typeof parsedEvent.status === "boolean"
+      ? parsedEvent.status
+      : typeof parsedEvent.status === "string"
+      ? parsedEvent.status.toLowerCase() === "true"
+      : false;
+
+  // 새로 생성
+  const timestamp = Math.floor(new Date().getTime() / 1000);
+  const tableName = process.env.DYNAMODB_TABLE_NAME_REGION || "Regions"; // 환경 변수 사용 권장
+
+  const params = {
+    TableName: tableName,
+    Key: {
+      region: region,
+    },
+    UpdateExpression: "SET #st = :newSt",
+    ExpressionAttributeNames: {
+      "#st": "status",
+    },
+    ExpressionAttributeValues: {
+      ":newSt": status,
+    },
+    ReturnValues: "ALL_NEW",
+  };
+
+  let dbResultStatus;
+  try {
+    await ddbDocClient.send(new UpdateCommand(params));
+    console.log(`✅ DynamoDB '${region}' 아이템의 status: ${status}`);
+    dbResultStatus = "성공";
+  } catch (error) {
+    console.error(`❌ DynamoDB '${region}' 아이템의 status:`, error.message);
+    dbResultStatus = "실패";
+  }
+  const statusCode = dbResultStatus === "성공" ? 200 : 500;
+  const message = dbResultStatus === "성공" ? "DynamoDB 저장 성공" : "DynamoDB 저장 실패";
+
+  return {
+    statusCode: statusCode,
+    body: JSON.stringify({
+      message: message,
+      dbStatus: dbResultStatus,
+      region: region,
+      status: status,
+      timestamp: timestamp,
+    }),
+  };
+};
